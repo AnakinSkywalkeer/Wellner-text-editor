bc630a8b71740155bdc9a584df440e96401573e7
AnakinSkywalkeer <gurmanavdeepsinghmahal9@gmail.com>
2023-05-03 19:16:49 +0530

    added the raw mode

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+struct termios orig_termios;
 
-void enableRawMode(){
-     struct termios raw;
-
+void disable_raw_mode(void) {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
-int main(){
-    char c;
-    while(read(STDIN_FILENO,&c,1)==1 && c!='q');
+
+void enable_raw_mode(void){
+     tcgetattr(STDIN_FILENO, &orig_termios);
+     atexit(disable_raw_mode);
+     struct termios raw=orig_termios;
+     raw.c_lflag &= ~(ECHO);
+     tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+}
+int main(void){
+      
+    uint8_t c;
+    uint8_t d='¶';
+    char y;
+    while(read(STDIN_FILENO, &c, 4) == 1 &&  c!=d);
+    // enable_raw_mode();
+    scanf("%c",&y);
+    printf("%c",y);
     return 0;
 }
